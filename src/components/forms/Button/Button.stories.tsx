3c22bd03af740e6ad800ffb339b793cceb969dcb
Michal Zajac <michal.99.zajac@gmail.com>
2021-11-06 17:45:14 +0100

    feat(config): add basic config

---
+++ src/components/forms/Button/Button.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Button from './Button'
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+}
+
+const Template = () => <Button />
+
+export const Default = Template.bind({})
