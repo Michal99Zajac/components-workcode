d6d4a1945ede8ab13a0944320eb8da4f0709a648
Michal Zajac <michal.99.zajac@gmail.com>
2021-11-08 19:49:10 +0100

    refactor(style): add style for disabled button

--- src/components/forms/Button/Button.stories.tsx
+++ src/components/forms/Button/Button.stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react'
-import { ButtonGroup } from '@chakra-ui/react'
+import { ButtonGroup, Stack } from '@chakra-ui/react'
 import React from 'react'
 
 import Button from './Button'
@@ -10,13 +10,43 @@ export const primaryButton: Story = () => (
 export const secondaryButton: Story = () => (
   <Button variant="secondary">Click Me!</Button>
 )
-export const sizes: Story = () => (
-  <ButtonGroup spacing={6}>
-    <Button size="lg">Click Me!</Button>
-    <Button size="md">Click Me!</Button>
-    <Button size="sm">Click Me!</Button>
-    <Button size="xs">Click Me!</Button>
+export const disabledButton: Story = () => (
+  <ButtonGroup>
+    <Button>Click Me!</Button>
+    <Button variant="secondary">Click Me!</Button>
   </ButtonGroup>
+)
+export const loadingButton: Story = () => (
+  <ButtonGroup>
+    <Button isLoading>Click Me!</Button>
+    <Button isLoading variant="secondary">
+      Click Me!
+    </Button>
+  </ButtonGroup>
+)
+export const sizes: Story = () => (
+  <Stack>
+    <ButtonGroup spacing={6}>
+      <Button size="lg">Click Me!</Button>
+      <Button size="md">Click Me!</Button>
+      <Button size="sm">Click Me!</Button>
+      <Button size="xs">Click Me!</Button>
+    </ButtonGroup>
+    <ButtonGroup spacing={6}>
+      <Button size="lg" variant="secondary">
+        Click Me!
+      </Button>
+      <Button size="md" variant="secondary">
+        Click Me!
+      </Button>
+      <Button size="sm" variant="secondary">
+        Click Me!
+      </Button>
+      <Button size="xs" variant="secondary">
+        Click Me!
+      </Button>
+    </ButtonGroup>
+  </Stack>
 )
 
 export default {
