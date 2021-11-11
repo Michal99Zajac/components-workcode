09f2d4773a24f7eb63e953c5119466a9ab71cfcc
Michal Zajac <michal.99.zajac@gmail.com>
2021-11-11 21:32:09 +0100

    feat(RingIcon): add RingIcon icon as component

---
+++ src/components/icons/RingIcon/RingIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createIcon } from '@chakra-ui/react'
+
+export const RingIcon = createIcon({
+  displayName: 'RingIcon',
+  viewBox: '0 0 18 20',
+  path: (
+    <path
+      fill="currentColor"
+      d="M11 2V0H7V2H2V4H16V2H11ZM16 14H2V10H0V16H5V20H7V16H11V18H7V20H13V16H18V10H16V4H14V12H16V14ZM2 4V12H4V4H2Z"
+    />
+  ),
+})
+
+export default RingIcon
---
+++ src/components/icons/RingIcon/index.ts
@@ -0,0 +1,2 @@
+export { default } from './RingIcon'
+export * from './RingIcon'
