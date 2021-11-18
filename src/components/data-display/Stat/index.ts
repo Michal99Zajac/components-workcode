e86af968f9a198b766858b391f29d6cfb631582b
Michal Zajac <michal.99.zajac@gmail.com>
2021-11-18 15:22:35 +0100

    feat(stat): add stat component

---
+++ src/components/data-display/Stat/index.ts
@@ -0,0 +1,6 @@
+export { Stat } from '@chakra-ui/react'
+export * from './StatArrow'
+export * from './StatGroup'
+export * from './StatHelpText'
+export * from './StatLabel'
+export * from './StatNumber'
