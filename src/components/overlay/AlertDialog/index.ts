934a3d6ecdafe59f862f46da70d262b11073e6cc
Michal Zajac <michal.99.zajac@gmail.com>
2021-11-18 17:01:46 +0100

    feat(alertDialog): add alertDialog component

---
+++ src/components/overlay/AlertDialog/index.ts
@@ -0,0 +1,6 @@
+export { AlertDialog } from '@chakra-ui/react'
+export * from './AlertDialogBody'
+export * from './AlertDialogContent'
+export * from './AlertDialogFooter'
+export * from './AlertDialogHeader'
+export * from './AlertDialogOverlay'
